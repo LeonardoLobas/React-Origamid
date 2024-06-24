af93bb2da0c4d273c3be87ea55cff7b2822f33f2
LeonardoLobas <leonardolobas2703@gmail.com>
2024-06-24 17:49:25 -0300

    React + Vite

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from 'react'
 
-const App = () => {
-  return <a href="https://www.origamid.com">Origamid</a>;
-};
+const App = () =>  {
+  return <div>App React</div>
+}
 
-export default App;
+export default App
