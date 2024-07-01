cd166d00ac86f569e16f400e001118fe368bece0
LeonardoLobas <leonardolobas2703@gmail.com>
2024-07-01 17:48:54 -0300

    Use state

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import Hooks from "./Hooks/hooks.jsx";
+import App from "./Aplicativo_projeto/App.jsx";
+import Hooks from "./Use_state/hooks.jsx";
 
 // ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 ReactDOM.createRoot(document.getElementById("root")).render(<Hooks />);
