0403dcd312766bb338c6b458574f60a6c537acc6
LeonardoLobas <leonardolobas2703@gmail.com>
2024-07-10 15:48:32 -0300

    Use Ref.

--- src/Use_Ref/Use_Ref.jsx
+++ src/Use_Ref/Use_Ref.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const Use_Ref = () => {
-    return <div></div>;
+    const [comentarios, setComentarios] = React.useState([]);
+    const [input, setInput] = React.useState("");
+    const inputElement = React.useRef();
+
+    function handleClick() {
+        setComentarios((comentarios) => [...comentarios, input]);
+        setInput("");
+        inputElement.current.focus();
+    }
+
+    return (
+        <div>
+            <ul>
+                {comentarios.map((comentario) => (
+                    <li key={comentario}>{comentario}</li>
+                ))}
+            </ul>
+            <input
+                type="text"
+                value={input}
+                ref={inputElement}
+                onChange={({ target }) => setInput(target.value)}
+            />
+            <br />
+            <button onClick={handleClick}>Enviar</button>
+        </div>
+    );
 };
 
 export default Use_Ref;
--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,10 @@ import App from "./Aplicativo_projeto/App.jsx";
 import Hooks from "./Use_state/hooks.jsx";
 import UseState_Exe from "./Use_state/UseState_Exe.jsx";
 import Use_Effect from "./Use_Effect/Use_Effect.jsx";
+import Use_Ref from "./Use_Ref/Use_Ref.jsx";
 
 // ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 // ReactDOM.createRoot(document.getElementById("root")).render(<Hooks />);
 // ReactDOM.createRoot(document.getElementById("root")).render(<UseState_Exe />);
-ReactDOM.createRoot(document.getElementById("root")).render(<Use_Effect />);
+// ReactDOM.createRoot(document.getElementById("root")).render(<Use_Effect />);
+ReactDOM.createRoot(document.getElementById("root")).render(<Use_Ref />);
