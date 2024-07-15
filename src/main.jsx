26b6279b28fe1b56145fc2ad82f88fdf160e8f2d
LeonardoLobas <leonardolobas2703@gmail.com>
2024-07-15 17:24:27 -0300

    Use memo.

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import Hooks from "./Use_state/hooks.jsx";
 import UseState_Exe from "./Use_state/UseState_Exe.jsx";
 import Use_Effect from "./Use_Effect/Use_Effect.jsx";
 import Use_Ref from "./Use_Ref/Use_Ref.jsx";
+import Use_Memo from "./Use_memo/use_callback/use_memo.jsx";
 
 // ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 // ReactDOM.createRoot(document.getElementById("root")).render(<Hooks />);
 // ReactDOM.createRoot(document.getElementById("root")).render(<UseState_Exe />);
 // ReactDOM.createRoot(document.getElementById("root")).render(<Use_Effect />);
-ReactDOM.createRoot(document.getElementById("root")).render(<Use_Ref />);
+// ReactDOM.createRoot(document.getElementById("root")).render(<Use_Ref />);
+ReactDOM.createRoot(document.getElementById("root")).render(<Use_Memo />);
