acd70518d00bb4cedaa5e51c03fdb1d4290a8fcd
LeonardoLobas <leonardolobas2703@gmail.com>
2024-07-17 17:51:33 -0300

    Use context.

--- src/Use_Context/Global_Context.jsx
+++ src/Use_Context/Global_Context.jsx
@@ -3,9 +3,20 @@ import React from "react";
 export const Global_Context = React.createContext();
 
 export const GlobalStorage = ({ children }) => {
-    const [carrinho, setCarrinho] = React.useState(0);
+    const [dados, setDados] = React.useState(null);
+
+    React.useEffect(() => {
+        const api = fetch("https://ranekapi.origamid.dev/json/api/produto/")
+            .then((response) => response.json())
+            .then((json) => setDados(json));
+    });
+
+    function limparDados() {
+        setDados(null);
+    }
+
     return (
-        <Global_Context.Provider value={{ carrinho, setCarrinho }}>
+        <Global_Context.Provider value={{ limparDados, dados, setDados }}>
             {children}
         </Global_Context.Provider>
     );
--- src/Use_Context/Itens.jsx
+++ src/Use_Context/Itens.jsx
@@ -3,16 +3,7 @@ import { Global_Context } from "./Global_Context";
 
 const Itens = () => {
     const global = React.useContext(Global_Context);
-
-    function handleClick() {
-        global.setCarrinho((carrinho) => carrinho + 1);
-    }
-    return (
-        <p>
-            Total: {global.carrinho}:{" "}
-            <button onClick={handleClick}>Adicionar</button>
-        </p>
-    );
+    return <div>Produto:</div>;
 };
 
 export default Itens;
