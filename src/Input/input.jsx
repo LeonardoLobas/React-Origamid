e4ed9852246cfd07c9b965eef1a006a34041a52c
LeonardoLobas <leonardolobas2703@gmail.com>
2024-07-25 17:52:50 -0300

    Input.

--- src/Input/input.jsx
+++ src/Input/input.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 const Input = () => {
     const [nome, setNome] = React.useState(" ");
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(event);
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="nome">Nome</label>
             <input
                 type="text"
@@ -10,6 +14,7 @@ const Input = () => {
                 value={nome}
                 onChange={(event) => setNome(event.target.value)}
             />
+            <button>Enviar</button>
             <p>{nome}</p>
         </form>
     );
