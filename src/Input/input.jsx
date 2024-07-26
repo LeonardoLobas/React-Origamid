f3ccb74c4c712aa24ad091ecd8ee75b7f3c8ca82
LeonardoLobas <leonardolobas2703@gmail.com>
2024-07-26 17:44:11 -0300

    input com objetos.

--- src/Input/input.jsx
+++ src/Input/input.jsx
@@ -1,21 +1,41 @@
 import React from "react";
+
+const formFields = [
+    { id: "nome", label: "Nome", type: "text" },
+    { id: "email", label: "Email", type: "email" },
+    { id: "senha", label: "Senha", type: "password" },
+];
+
 const Input = () => {
-    const [nome, setNome] = React.useState(" ");
-    function handleSubmit(event) {
-        event.preventDefault();
-        console.log(event);
+    const [form, setForm] = React.useState({
+        nome: "",
+        email: "",
+        senha: "",
+        cep: "",
+        rua: "",
+        bairro: "",
+        cidade: "",
+        estado: "",
+    });
+
+    function handleChange({ target }) {
+        const { id, value } = target;
+        setForm({ ...form, [id]: value });
     }
+
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="nome">Nome</label>
-            <input
-                type="text"
-                id="nome"
-                value={nome}
-                onChange={(event) => setNome(event.target.value)}
-            />
-            <button>Enviar</button>
-            <p>{nome}</p>
+        <form>
+            {formFields.map(({ id, label, type }) => (
+                <div>
+                    <label htmlFor={id}>{label}</label>
+                    <input
+                        type={type}
+                        id={id}
+                        value={form[id]}
+                        onChange={handleChange}
+                    />
+                </div>
+            ))}
         </form>
     );
 };
