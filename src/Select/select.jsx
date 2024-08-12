b842967decd466e85b7c54638baf3d6008442753
LeonardoLobas <leonardolobas2703@gmail.com>
2024-08-12 17:51:10 -0300

    Select componentes

--- src/Select/select.jsx
+++ src/Select/select.jsx
@@ -1,20 +1,34 @@
 import React from "react";
+import ComponentesSelect from "./componentesSelect";
+// const Select = () => {
+//     const [select, setSelect] = React.useState("");
+//     return (
+//         <form>
+//             <select
+//                 value={select}
+//                 onChange={({ target }) => setSelect(target.value)}
+//             >
+//                 <option disabled value=""></option>
+//                 <option value="notebook">Notebook</option>
+//                 <option value="smartphone">Smartphone</option>
+//                 <option value="trablet">Tablet</option>
+//             </select>
+//             {select}
+//         </form>
+//     );
+// };
 
 const Select = () => {
-    const [select, setSelect] = React.useState("");
+    const [produto, setProduto] = React.useState("");
     return (
         <form>
-            <select
-                value={select}
-                onChange={({ target }) => setSelect(target.value)}
-            >
-                <option disabled value=""></option>
-                <option value="notebook">Notebook</option>
-                <option value="smartphone">Smartphone</option>
-                <option value="trablet">Tablet</option>
-            </select>
-            {select}
+            <ComponentesSelect
+                options={["Notebook", "Smartphone", "Tablet"]}
+                value={produto}
+                setValue={setProduto}
+            />
         </form>
     );
 };
+
 export default Select;
